977c68cd3ff25730d1a7fa68a17412718f3b6e6a
FELIPE NOGUEIRA <felipenogueira@bocombbm.com.br>
2023-04-05 19:25:18 -0300

    Update 1.1 - component's dividers implemented and now it can be used to build more complex emails. yet to test with a more complex template

--- Components/Dividers/Abstract/HtmlDivider.cs
+++ Components/Dividers/Abstract/HtmlDivider.cs
@@ -1,6 +1,5 @@
 ﻿using Html.Components.Abstract;
 using Html.Interfaces;
-using Microsoft.AspNetCore.Html;
 using System.Web.Mvc;
 
 namespace Html.Components.Dividers.Abstract
@@ -15,13 +14,15 @@ namespace Html.Components.Dividers.Abstract
 
         public string EndTag => TagBuilder.ToString(TagRenderMode.EndTag);
 
-        public IHtmlContent StartTagContent => new HtmlString(StartTag);
-
-        public IHtmlContent EndTagContent => new HtmlString(EndTag);
-
         /// <summary>
         /// A Divider can Have multiple Html Components including other Dividers
         /// </summary>
         public abstract IEnumerable<IHtmlComponent> ChildComponents { get; }
+
+        /// <summary>
+        /// Appends to Components InnerText, implementation should consider IHtmlComponent implementation
+        /// </summary>
+        /// <param name="text"></param>
+        public void AppendToInnerText(string text) { TagBuilder.InnerHtml = TagBuilder.InnerHtml.Concat(text).ToString(); }
     }
 }
